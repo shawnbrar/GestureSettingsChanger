52dab063aaeb60ce3e83b3a70109a6a3450d1c39
Sudhakar Brar <dxhrmhall1449@tutanota.com>
2023-01-29 15:21:42 +0100

    Made changes to app.py

--- app.py
+++ app.py
@@ -7,15 +7,18 @@ import gestureVolumeChanger
 
 cap = cv2.VideoCapture(-1)
 # gesture volume change - always to be declared outside the while loop
-volcontrol = volcontrol_module.gestureVolumeChanger("./gesture_recognizer.task", 60) 
+gestVolControl = gestureVolumeChanger.gestureVolumeChanger("./gesture_recognizer.task", 60) 
 
 while True:
     success, img = cap.read()
     img = cv2.resize(img, (320, 240))
-    volcontrol.gestureRecognize(img)
-    volcontrol.changeVolume(img)
+    # First recognize the gesture
+    gestVolControl.gestureRecognize(img)
+    # Second change the volume
+    gestVolControl.changeVolume(img)
     cv2.imshow("Image", img)
     key = cv2.waitKey(1) & 0xFF
+    # Press q to quit
     if key == ord('q'):
         break
 
