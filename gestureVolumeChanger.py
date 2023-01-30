dd2cb6bad29a04f708bfcf5a5ae000c387e73619
Sudhakar Brar <dxhrmhall1449@tutanota.com>
2023-01-30 12:29:30 +0100

    Right hand detection for Gesture Volume Changer to work

--- gestureVolumeChanger.py
+++ gestureVolumeChanger.py
@@ -34,7 +34,8 @@ class gestureVolumeChanger:
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         if self.gestureResult != 'Closed_Fist':
             results = self.hands.process(imgRGB)
-            if results.multi_hand_landmarks:
+            # Right hand is detected as left
+            if results.multi_handedness is not None and results.multi_handedness[0].classification[0].label == 'Left':
                 thumb = results.multi_hand_landmarks[0].landmark[4]
                 index = results.multi_hand_landmarks[0].landmark[8]
                 thumb = {'x': round(thumb.x * img.shape[1]), 'y': round(thumb.y * img.shape[0])}
